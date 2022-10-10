12be3c015f2d670b523c69d39d16c121692b2b27
KarimPucci <kizpucci@gmail.com>
2022-10-10 15:55:33 +0200

    start

---
+++ js/script.js
@@ -0,0 +1,16 @@
+console.log('ciao Karim');
+
+//dichiaro le costanti e variabili.
+const nomeUtente = "Karim";
+const cognomeUtente = "Pucci";
+const colorePreferito = "Grigio"
+let output;
+//chiuso
+
+
+const nomeUtente = (prompt("What is your name?"));
+const cognomeUtente = (prompt("What is your last name?"));
+const colorePreferito = (prompt("What is your favorite color?"));
+output = `
+  <strong> ${nomeUtente} </strong> <strong> ${cognomeUtente} </strong> <strong> ${colorePreferito} </strong>
+`
