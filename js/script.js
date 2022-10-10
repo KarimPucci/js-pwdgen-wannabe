72dbfb839c67cd3216da9d86bb47c257845dc31c
KarimPucci <kizpucci@gmail.com>
2022-10-10 17:50:03 +0200

    finish

--- js/script.js
+++ js/script.js
@@ -12,6 +12,9 @@ nomeUtente = prompt("What is your name?");
 cognomeUtente = (prompt("What is your last name?"));
 colorePreferito = (prompt("What is your favorite color?"));
 output = `
-  <strong> ${nomeUtente} </strong> <strong> ${cognomeUtente} </strong> <strong> ${colorePreferito} </strong>
+  <strong> <span>Name entered:</span> ${nomeUtente} </strong> <br>
+  <strong> <span>Last Name entered:</span> ${cognomeUtente} </strong> <br>
+  <strong> <span>Color entered:</span> ${colorePreferito} </strong> <br>
+  <strong> <span>Your Password: KarimPucci22</span></strong>
 `
 document.getElementById("output").innerHTML = output
